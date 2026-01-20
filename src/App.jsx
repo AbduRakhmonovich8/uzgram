0dd1de31e2bcb29967ae792e3f7c3c4aa8ea05a1
AbuZar <xabzarbek.com>
2026-01-20 22:44:00 +0500

    dsfsdfs

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
     setStatus(u ? "Logged in (Telegram WebApp)" : "User topilmadi (initDataUnsafe.user yo‘q)");
   }, [tg]);
 
-  alert(user)
+  alert(user?.username)
 
 
 
