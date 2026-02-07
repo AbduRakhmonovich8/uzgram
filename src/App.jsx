b64213622e20dcd3f4490dbe5fb36d47970b632c
AbuZar <xabzarbek.com>
2026-02-07 13:21:24 +0500

    dfdssdv/;

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   const [activeNumbers, setActiveNumbers] = useState([]);
   const [status, setStatus] = useState("Initializing...");
 
-  alert("yangilanish v = 2")
+  alert("yangilanish v = 33")
 
   useEffect(() => {
     WebApp.ready();      // 🔑 muhim
